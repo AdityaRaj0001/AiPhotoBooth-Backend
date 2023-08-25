eeda1515c7654eeb97661b9a78e6af1652004271
S1D007 <s1d.official011001011@gmail.com>
2023-08-25 18:58:19 +0530

    Fixed some issue and Added BG CHANGER

--- src/BgChanger/controller/bg.controller.ts
+++ src/BgChanger/controller/bg.controller.ts
@@ -4,13 +4,15 @@ import axios from "axios";
 import { toB64 } from "../../utils/bs64";
 import FormData from "form-data";
 
+const image2 = "./src/assets/image2.png";
+const image3 = "./src/assets/image3.png";
+
 const Bg_Changer = async (req: Request, res: Response) => {
     try {
         const files = req.files as {
             [fieldname: string]: Express.Multer.File[];
         };
-
-        const mainImageBuffer = files.bg[0].buffer;
+        // console.log(files)
         const overlayImageBuffer = files.input_image[0].buffer;
 
         const SG_IMAGE_DATA = new FormData();
@@ -21,28 +23,38 @@ const Bg_Changer = async (req: Request, res: Response) => {
                 'x-api-key': process.env.PIC_WISH_API_KEY,
             },
         });
+        console.log(bgRemovedOverlayImageBuffer.data.data.image)
         const data_bgRemovedOverlayImageBuffer = await axios.get(bgRemovedOverlayImageBuffer.data.data.image, { responseType: 'arraybuffer' })
         const imageBuffer = Buffer.from(data_bgRemovedOverlayImageBuffer.data, 'binary');
         const overlayMetadata = await sharp(imageBuffer).metadata();
-        const overlayWidth = Math.floor(overlayMetadata.width || 0 * 0.8);
-        const overlayHeight = Math.floor(overlayMetadata.height || 0 * 0.8);
-        const mainMetadata = await sharp(mainImageBuffer).metadata();
+        const overlayWidth = Math.floor(overlayMetadata.width || 0 * 0.5);
+        const overlayHeight = Math.floor(overlayMetadata.height || 0 * 0.5);
+        const mainMetadata = await sharp(image2).metadata();
         const mainWidth = mainMetadata.width || 0;
         const mainHeight = mainMetadata.height || 0;
-        const overlayX = (mainWidth - overlayWidth) - 400;
-        const overlayY = (mainHeight - overlayHeight) - 100;
+        const overlayX = (mainWidth - overlayWidth) - 370;
+        const overlayY = (mainHeight - overlayHeight) - 130;
 
         if (overlayWidth <= mainWidth && overlayHeight <= mainHeight) {
             const resizedOverlayBuffer = await sharp(imageBuffer)
                 .resize(overlayWidth, overlayHeight)
                 .toBuffer();
 
-            const compositeBuffer = await sharp(mainImageBuffer)
+            const compositeBuffer = await sharp(image2)
                 .composite([{ input: resizedOverlayBuffer, left: overlayX, top: overlayY, gravity: 'southeast' }])
                 .toBuffer();
 
-            // res.set('Content-Type', 'image/jpeg');
-            res.send(compositeBuffer);
+            const baseImageBuffer = await sharp(Buffer.from(compositeBuffer))
+                .resize(600, 400)
+                .toBuffer()
+            const overlayImageBuffer = await sharp(image3)
+                .resize(600, 400)
+                .toBuffer();
+            const compositeImageBuffer = await sharp(baseImageBuffer)
+                .composite([{ input: overlayImageBuffer }])
+                .toBuffer();
+
+            res.send(compositeImageBuffer);
         } else {
             res.status(400).send('Overlay image dimensions are too large');
         }
