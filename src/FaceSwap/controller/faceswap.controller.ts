2f707497db476665d389b7646495fa4a11f30727
S1D007 <s1d.official011001011@gmail.com>
2023-08-25 17:51:39 +0530

    : )

--- src/FaceSwap/controller/faceswap.controller.ts
+++ src/FaceSwap/controller/faceswap.controller.ts
@@ -1,12 +1,31 @@
 import { toB64 } from "../../utils/bs64";
 import axios from "axios";
 import { Request, Response } from "express";
+import sharp from "sharp";
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+
+const overlayImagePath = './src/assets/image.png';
+cloudinary.config({
+    cloud_name: 'dxjk4gnrw',
+    api_key: '848441943216717',
+    api_secret: 'DKKpZ5zkG7zuwBm0xXmPQ7Dyj5E'
+});
+
+async function uploadImage(buffer: Buffer, folder: string) {
+    try {
+        await cloudinary.uploader.upload_stream({ folder }, (error: any, result: any) => {
+        }).end(buffer);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
 const SwapFace = async (req: Request, res: Response) => {
     try {
         const files = req.files as {
             [fieldname: string]: Express.Multer.File[];
         }
-        
         const API_KEY = process.env.API_KEY;
 
         const data = {
@@ -21,8 +40,17 @@ const SwapFace = async (req: Request, res: Response) => {
             },
             responseType: 'arraybuffer'
         });
+        const baseImageBuffer = await sharp(Buffer.from(response.data))
+            .resize(400, 600)
+            .toBuffer()
+        const overlayImageBuffer = await sharp(overlayImagePath)
+            .resize(400, 600)
+            .toBuffer();
+        const compositeImageBuffer = await sharp(baseImageBuffer)
+            .composite([{ input: overlayImageBuffer }])
+            .toBuffer();
         const SG_IMAGE_DATA = new FormData();
-        SG_IMAGE_DATA.append('image_base64', toB64(response.data));
+        SG_IMAGE_DATA.append('image_base64', toB64(compositeImageBuffer));
         SG_IMAGE_DATA.append('fix_face_only', '1');
         SG_IMAGE_DATA.append('type', 'face');
         SG_IMAGE_DATA.append('scale_factor', '4');
@@ -33,7 +61,21 @@ const SwapFace = async (req: Request, res: Response) => {
                 'x-api-key': process.env.PIC_WISH_API_KEY
             }
         });
-        res.send(EnhancedImage.data.data);
+        const enhancedImageResponse = await axios.get(EnhancedImage.data.data.image, {
+            responseType: 'arraybuffer'
+        });
+        const folderName = new Date().getTime().toString();
+        const uploadPromises = [
+            uploadImage(Buffer.from(files.input_image[0].buffer), folderName),
+            uploadImage(enhancedImageResponse.data, folderName)
+        ];
+        const ResponseData = await Promise.all(uploadPromises);
+        if (ResponseData) {
+            res.setHeader('Content-Type', 'image/jpeg');
+            res.send(EnhancedImage.data.data);
+        } else {
+            res.status(500).json({ error: "Internal Server Error" });
+        }
     } catch (error: any) {
         console.error(error.response);
         res.status(500).json({ error: error.message || "Internal Server Error" });
