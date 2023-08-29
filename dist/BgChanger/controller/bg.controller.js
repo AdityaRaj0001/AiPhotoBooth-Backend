07b0c9c516090ed75e1bde9249697f56db3f3eaa
Siddhant <94226898+S1D007@users.noreply.github.com>
2023-08-29 04:17:19 +0000

    hey

--- dist/BgChanger/controller/bg.controller.js
+++ dist/BgChanger/controller/bg.controller.js
@@ -4,49 +4,89 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const sharp_1 = __importDefault(require("sharp"));
+const axios_1 = __importDefault(require("axios"));
+const bs64_1 = require("../../utils/bs64");
+const form_data_1 = __importDefault(require("form-data"));
+const faceswap_controller_1 = require("../../FaceSwap/controller/faceswap.controller");
+const image2 = "./src/assets/image2.png";
+const image3 = "./src/assets/image3.png";
 const Bg_Changer = async (req, res) => {
     try {
-        console.log(req.files);
         const files = req.files;
-        const mainImageBuffer = files.bg[0].buffer; // Buffer for the beautiful background image
-        const overlayImageBuffer = files.input_image[0].buffer; // Buffer for the image with transparent background
-        // Load overlay image dimensions
-        const overlayMetadata = await (0, sharp_1.default)(overlayImageBuffer).metadata();
-        let overlayWidth = overlayMetadata.width || 0;
-        let overlayHeight = overlayMetadata.height || 0;
-        // Calculate new dimensions for the overlay image (1.5 times bigger)
-        overlayWidth *= 0.5;
-        overlayHeight *= 0.5;
-        // Round down the calculated dimensions to the nearest integer
-        overlayWidth = Math.floor(overlayWidth);
-        overlayHeight = Math.floor(overlayHeight);
-        // Calculate the desired position for overlay (right-aligned)
-        const mainMetadata = await (0, sharp_1.default)(mainImageBuffer).metadata();
+        // console.log(files)
+        const overlayImageBuffer = files.input_image[0].buffer;
+        const SG_IMAGE_DATA = new form_data_1.default();
+        SG_IMAGE_DATA.append('image_base64', (0, bs64_1.toB64)(overlayImageBuffer));
+        SG_IMAGE_DATA.append('sync', '1');
+        const bgRemovedOverlayImageBuffer = await axios_1.default.post(process.env.PIC_WISH_BASE_URL_BG_REMOVE || "", SG_IMAGE_DATA, {
+            headers: {
+                'x-api-key': process.env.PIC_WISH_API_KEY,
+            },
+        });
+        console.log(bgRemovedOverlayImageBuffer.data.data.image);
+        const data_bgRemovedOverlayImageBuffer = await axios_1.default.get(bgRemovedOverlayImageBuffer.data.data.image, { responseType: 'arraybuffer' });
+        const imageBuffer = Buffer.from(data_bgRemovedOverlayImageBuffer.data, 'binary');
+        const overlayMetadata = await (0, sharp_1.default)(imageBuffer).metadata();
+        // increase the height and width 
+        const overlayWidth = Math.floor((overlayMetadata.width || 0) * 2);
+        const overlayHeight = Math.floor((overlayMetadata.height || 0) * 2);
+        const mainMetadata = await (0, sharp_1.default)(image2).metadata();
         const mainWidth = mainMetadata.width || 0;
         const mainHeight = mainMetadata.height || 0;
-        const overlayX = mainWidth - overlayWidth; // X-coordinate
-        const overlayY = mainHeight - overlayHeight; // Y-coordinate
-        // Check if the overlay image dimensions are smaller than the main image
+        const overlayX = (mainWidth - overlayWidth) + 200;
+        const overlayY = (mainHeight - overlayHeight) - 140;
         if (overlayWidth <= mainWidth && overlayHeight <= mainHeight) {
-            // Resize overlay image to the new dimensions
-            const resizedOverlayBuffer = await (0, sharp_1.default)(overlayImageBuffer)
+            const resizedOverlayBuffer = await (0, sharp_1.default)(imageBuffer)
                 .resize(overlayWidth, overlayHeight)
                 .toBuffer();
-            // Compose images
-            const compositeBuffer = await (0, sharp_1.default)(mainImageBuffer)
+            const compositeBuffer = await (0, sharp_1.default)(image2)
                 .composite([{ input: resizedOverlayBuffer, left: overlayX, top: overlayY, gravity: 'southeast' }])
                 .toBuffer();
-            // res.set('Content-Type', 'image/jpeg'); // Adjust the content type based on your output format
-            res.send(compositeBuffer);
+            const [baseImageBuffer, overlayImageBuffer] = await Promise.all([
+                (0, sharp_1.default)(Buffer.from(compositeBuffer))
+                    .resize(600, 400)
+                    .toBuffer(),
+                (0, sharp_1.default)(image3)
+                    .resize(600, 400)
+                    .toBuffer()
+            ]);
+            const compositeImageBuffer = await (0, sharp_1.default)(baseImageBuffer)
+                .composite([{ input: overlayImageBuffer }])
+                .toBuffer();
+            const data = {
+                "image": (0, bs64_1.toB64)(compositeImageBuffer),
+                "scale": 8
+            };
+            const [_, responseData] = await Promise.all([
+                (0, sharp_1.default)(baseImageBuffer)
+                    .composite([{ input: overlayImageBuffer }])
+                    .toBuffer(),
+                axios_1.default.post(process.env.ENHANCE_BASE_URL || "", data, {
+                    headers: {
+                        'x-api-key': process.env.API_KEY,
+                    },
+                    responseType: 'arraybuffer'
+                })
+            ]);
+            const folderName = new Date().getTime().toString();
+            const uploadPromises = [
+                (0, faceswap_controller_1.uploadImage)(Buffer.from(files.input_image[0].buffer), folderName),
+                (0, faceswap_controller_1.uploadImage)(responseData.data, folderName)
+            ];
+            const uploadResults = await Promise.all(uploadPromises);
+            // console.log(uploadResults);
+            // res.setHeader('Content-Type', 'image/png');
+            res.send({
+                image: uploadResults[1].secure_url,
+            });
         }
         else {
-            // Handle the case where the overlay image is larger than the main image
             res.status(400).send('Overlay image dimensions are too large');
         }
     }
     catch (e) {
-        // Handle errors
-        console.error(e);
+        console.log(e);
+        // res.send(e.response.data);
         res.status(500).send('An error occurred');
     }
 };
--- dist/FaceSwap/controller/faceswap.controller.js
+++ dist/FaceSwap/controller/faceswap.controller.js
@@ -3,8 +3,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.uploadImage = void 0;
 const bs64_1 = require("../../utils/bs64");
 const axios_1 = __importDefault(require("axios"));
+const sharp_1 = __importDefault(require("sharp"));
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+const overlayImagePath = './src/assets/image.png';
+cloudinary.config({
+    cloud_name: 'dxjk4gnrw',
+    api_key: '848441943216717',
+    api_secret: 'DKKpZ5zkG7zuwBm0xXmPQ7Dyj5E'
+});
+async function uploadImage(buffer, folder) {
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader.upload_stream({ folder }, (error, result) => {
+            if (error) {
+                console.error(error);
+                reject(error);
+            }
+            else {
+                console.log(result);
+                resolve(result);
+            }
+        }).end(buffer);
+    });
+}
+exports.uploadImage = uploadImage;
 const SwapFace = async (req, res) => {
     try {
         const files = req.files;
@@ -20,19 +45,46 @@ const SwapFace = async (req, res) => {
             },
             responseType: 'arraybuffer'
         });
-        const SG_IMAGE_DATA = new FormData();
-        SG_IMAGE_DATA.append('image_base64', (0, bs64_1.toB64)(response.data));
-        SG_IMAGE_DATA.append('fix_face_only', '1');
-        SG_IMAGE_DATA.append('type', 'face');
-        SG_IMAGE_DATA.append('scale_factor', '4');
-        SG_IMAGE_DATA.append('return_type', '1');
-        SG_IMAGE_DATA.append('sync', '1');
-        const EnhancedImage = await axios_1.default.post(process.env.PIC_WISH_BASE_URL || "", SG_IMAGE_DATA, {
+        const [baseImageBuffer, overlayImageBuffer] = await Promise.all([
+            (0, sharp_1.default)(Buffer.from(response.data, 'binary'))
+                .resize(400, 600)
+                .toBuffer(),
+            (0, sharp_1.default)(overlayImagePath)
+                .resize(400, 600)
+                .toBuffer()
+        ]);
+        const compositeImageBuffer = await (0, sharp_1.default)(baseImageBuffer)
+            .composite([{ input: overlayImageBuffer }])
+            .toBuffer();
+        const PW_IMAGE_DATA = new FormData();
+        PW_IMAGE_DATA.append('image_base64', (0, bs64_1.toB64)(compositeImageBuffer));
+        PW_IMAGE_DATA.append('fix_face_only', '1');
+        PW_IMAGE_DATA.append('type', 'face');
+        PW_IMAGE_DATA.append('scale_factor', '4');
+        PW_IMAGE_DATA.append('return_type', '1');
+        PW_IMAGE_DATA.append('sync', '1');
+        const EnhancedImage = await axios_1.default.post(process.env.PIC_WISH_BASE_URL || "", PW_IMAGE_DATA, {
             headers: {
                 'x-api-key': process.env.PIC_WISH_API_KEY
             }
         });
-        res.send(EnhancedImage.data.data);
+        const enhancedImageResponse = await axios_1.default.get(EnhancedImage.data.data.image, {
+            responseType: 'arraybuffer'
+        });
+        const folderName = new Date().getTime().toString();
+        const uploadPromises = [
+            uploadImage(Buffer.from(files.input_image[0].buffer), folderName),
+            uploadImage(enhancedImageResponse.data, folderName)
+        ];
+        const ResponseData = await Promise.all(uploadPromises);
+        if (ResponseData) {
+            res.send({
+                image: ResponseData[1].secure_url,
+            });
+        }
+        else {
+            res.status(500).json({ error: "Internal Server Error" });
+        }
     }
     catch (error) {
         console.error(error.response);
